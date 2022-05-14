d0f97c6f3999b1d0ae021e1ae03e43a27558cccf
ItzHefe <jeffrey.jarvis32@gmail.com>
2022-05-13 22:29:38 -0400

    updated index.js main file

--- index.js
+++ index.js
@@ -3,19 +3,23 @@ const db = require('./config/connection');
 const routes = require('./routes');
 const mongoose = require('mongoose');
 
-const cwd = process.cwd();
-
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-// Note: not necessary for the Express server to function. This just helps indicate what activity's server is running in the terminal.
-const activity = cwd.includes('01-Activities')
-  ? cwd.split('/01-Activities/')[1]
-  : cwd;
-
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(require('./routes'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-API', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
 
 db.once('open', () => {
   app.listen(PORT, () => {
